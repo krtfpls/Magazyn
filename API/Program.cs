83d5f5b1a722d4e6e48b5ca4d7d31ae5afa74e74
krtfpls <krtfpls@gmail.com>
2022-05-08 19:47:15 +0200

    first

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,12 @@
+using System.Reflection;
+using API.Extensions;
+using Application.Core;
+using Application.Products;
+using Data;
+using MediatR;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +15,24 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(opts =>{
+    opts.UseSqlite(
+        builder.Configuration["ConnectionStrings:SqliteConnection"]
+    );
+});
+//dodane
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession();
+
+// Mediator CQRS
+builder.Services.AddMediatR(typeof(List.Handler).Assembly);
+//AutoMapper
+builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 
 var app = builder.Build();
+
+// ExceptionMiddleware- Handle logs
+app.UseMiddleware<ExceptionMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -16,10 +41,37 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//added by myself
+app.UseRouting();
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
+//added end
+
+app.UseHttpsRedirection();
 app.UseAuthorization();
 
+// to remove
 app.MapControllers();
 
-app.Run();
+// handle fallback to index
+//   app.UseEndpoints(endpoints =>
+//             {
+//                 endpoints.MapControllers();
+//                 endpoints.MapFallbackToController("Index", "Fallback");
+//             });
+
+//SeedData
+using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+try{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
+}
+catch (Exception ex){
+    var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occured during migration");
+}
+
+await app.RunAsync();
