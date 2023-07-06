61df67cae6209e76cb51081eff1e2f3ef6d1e4b6
krtfpls <krtfpls@gmail.com>
2023-07-06 14:26:03 +0200

    db_change

--- API/Program.cs
+++ API/Program.cs
@@ -30,29 +30,29 @@ builder.Services.AddSwaggerGen();
 
 
 var connString = "";
-if (builder.Environment.IsDevelopment())
+// if (builder.Environment.IsDevelopment())
     connString= builder.Configuration.GetConnectionString("PostgresConnection");
      // builder.Configuration.GetConnectionString("SqliteConnection");
-else
-{
-  //  connString = builder.Configuration.GetConnectionString("PostgresConnection");
+// else
+// {
+//   //  connString = builder.Configuration.GetConnectionString("PostgresConnection");
 
-    // Use connection string provided at runtime by FlyIO.
-    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+//     // Use connection string provided at runtime by FlyIO.
+//     var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 
-    // Parse connection URL to connection string for Npgsql
-    connUrl = connUrl.Replace("postgres://", string.Empty);
-    var pgUserPass = connUrl.Split("@")[0];
-    var pgHostPortDb = connUrl.Split("@")[1];
-    var pgHostPort = pgHostPortDb.Split("/")[0];
-    var pgDb = pgHostPortDb.Split("/")[1];
-    var pgUser = pgUserPass.Split(":")[0];
-    var pgPass = pgUserPass.Split(":")[1];
-    var pgHost = pgHostPort.Split(":")[0];
-    var pgPort = pgHostPort.Split(":")[1];
+//     // Parse connection URL to connection string for Npgsql
+//     connUrl = connUrl.Replace("postgres://", string.Empty);
+//     var pgUserPass = connUrl.Split("@")[0];
+//     var pgHostPortDb = connUrl.Split("@")[1];
+//     var pgHostPort = pgHostPortDb.Split("/")[0];
+//     var pgDb = pgHostPortDb.Split("/")[1];
+//     var pgUser = pgUserPass.Split(":")[0];
+//     var pgPass = pgUserPass.Split(":")[1];
+//     var pgHost = pgHostPort.Split(":")[0];
+//     var pgPort = pgHostPort.Split(":")[1];
 
-    connString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}; SslMode=disable";
-}
+//     connString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}; SslMode=disable";
+// }
 builder.Services.AddDbContext<DataContext>(
     opt =>
 {
